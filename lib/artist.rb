bd475b9374da80ef4fd1c48ecdb62debf7a7e18a
Neena van de Water <nkuriakose@hotmail.com>
2018-03-14 19:10:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_accessor :name, :artist, :song
 
   @@song_count = 0
-  
+
   def initialize(name)
     @name = name
     @songs = []
@@ -13,8 +13,8 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
+    @songs << song
   end
 
 
