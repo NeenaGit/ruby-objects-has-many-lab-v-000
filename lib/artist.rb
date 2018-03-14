eb51f0b1a5404abe57f2eeb0d290c3dded5f20ce
Neena van de Water <nkuriakose@hotmail.com>
2018-03-14 16:36:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,16 +5,17 @@ class Artist
     @name = name
     @songs = []
   end
-
-  def add_song(song)
-    @songs << song
-    song = Song.new(song)
-    song.artist = self
-  end
-
+  
   def songs
     @songs
   end
+
+  def add_song(song)
+    song = Song.new(song)
+    @songs << song
+    song.artist = self
+  end
+
 
   def add_song_by_name(name)
     song = Song.new(name)
